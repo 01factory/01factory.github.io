c24b5761e7af03f236f5435635b886a248c4a87d
taoqf <tao_qiufeng@126.com>
2021-04-14 09:39:04 +0800

    test np

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
 	target: 'serverless',
 
-	pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
+	pageExtensions: ['mdx', 'md', 'ts', 'tsx'],
 
 	cssModules: true,
 
